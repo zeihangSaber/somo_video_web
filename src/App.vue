e4d7d6f0c87867d20d45420ab3575f7a46d578bc
zeihang <zeihang@sina.com>
2019-11-29 19:33:05 +0800

    fate

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default class App extends Vue {
 	beforeCreate() {
 		this.$router.beforeEach((to, from, next) => {
 			this.footShow = needFooter.includes(to.name as string);
-			console.log(this.footShow);
+			this.elMain = this.$route.name === "home" ? "active" : "";
 			next();
 		});
 	}

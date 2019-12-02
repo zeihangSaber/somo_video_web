487fcbaa985cf0293e417f573ce915d3525eb913
wangning <wangning@somo.tech>
2019-12-02 15:09:12 +0800

    活动页面

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    configureWebpack: {
-        devServer: {
-            proxy: {
-                "/vmt": {
-                    target: "https://vmt.video.somo.tech/api/v1"
-                },
-                "/udb": {
-                    target: "https://udb.video.somo.tech/api/v1"
-                },
-                "/bill": {
-                    target: "https://billing.video.somo.tech/api/v1"
-                }
-            }
-        },
-        externals: {
-            arg: "AgoraRTC"
-        }
-    }
+	configureWebpack: {
+		devServer: {
+			proxy: {
+				"/vmt": {
+					target: "https://vmt.video.somo.tech/api/v1"
+				},
+				"/udb": {
+					target: "https://udb.video.somo.tech/api/v1"
+				},
+				"/bill": {
+					target: "https://billing.video.somo.tech/api/v1"
+				}
+			}
+		},
+		externals: {
+			arg: "AgoraRTC"
+		}
+	}
 };

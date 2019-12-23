39624f160ca70ad7ac2afbb0be65d6d9c44eb34a
王伟忠 <601289650@qq.com>
2019-12-23 16:11:42 +0800

    12/23-7

--- src/components/controls.vue
+++ src/components/controls.vue
@@ -150,6 +150,7 @@
 							 + '会议链接：http://www.somo.tech/openApp?invite_code=' + this.shareData.mid +  '\n'
 							 + '您可以直接输入会议号加入会议， 也可以点击会议链接直接入会。';
 			setInterval(() => {
+					// console.log(this.data)
 					let timestamp = (new Date()).getTime();//当前时间戳
 					this.time =  timestamp - this.data.start;
 					this.time_meeting = this.formatDuring(this.time)

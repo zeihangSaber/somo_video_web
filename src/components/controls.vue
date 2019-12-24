4485ad78a13b8a7fdc6520fca694c735cf2d1bc2
zeihang <zeihang@sina.com>
2019-12-24 15:20:18 +0800

    123

--- src/components/controls.vue
+++ src/components/controls.vue
@@ -5,15 +5,15 @@
 			<div class="center">
 				<span class="bigSpan">会议号：{{this.data.code}}</span>
 				<span class="bigSpan" v-if="data.pwd">密码：{{this.data.pwd}}</span>
-				<span>{{this.data.name}}</span>
+				<span>
+					{{this.data.name ? this.data.name : "没有名字哦"}}
+					<i v-if="this.data.locked" class="font_family icon-lock"></i>
+				</span>
 				<span>
 					<i class="font_family icon-people-num"></i>
 					{{this.peopleNum}}
 				</span>
 				<span v-if="!this.data.muteAll">全体静音</span>
-				<span>
-					<i :class="`font_family icon-${this.data.locked ? 'lock' : 'unlock'}`"></i>
-				</span>
 				<span>
 					<i class="font_family icon-noMute"></i>
 					{{this.micNum}}

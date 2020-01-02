d0a6d321aa3cab3ca91de68dd37e677fcc34a6b7
zeihang <zeihang@sina.com>
2020-01-02 16:18:19 +0800

    kkk

--- src/components/player.vue
+++ src/components/player.vue
@@ -265,7 +265,7 @@
         position: absolute;
         top: 0;
         left: 0;
-        z-index: 55;
+        z-index: 2;
         background-color: #444;
         width: 100%;
         height: 100%;
